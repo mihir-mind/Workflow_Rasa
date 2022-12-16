965ec2261361469351c2c6ec4f4546c2170e4214
Mihir <mihir.kanteliya@mindinventory.com>
2022-12-16 16:08:25 +0530

    resolve socket error

--- workflow_rasa/actions/actions.py
+++ workflow_rasa/actions/actions.py
@@ -1,43 +1,34 @@
-# from rasa_sdk.forms import FormAction
 from typing import Text, List, Any, Dict, Union
 
-from rasa_sdk import Action, Tracker
-from rasa_sdk.forms import FormValidationAction
+from rasa_sdk import Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import UserUtteranceReverted
 from rasa_sdk.types import DomainDict
 
 
-class ValidateClientDetailsForm(FormValidationAction):
+class ValidateDetailsForm(FormValidationAction):
     def name(self) -> Text:
-        return "validate_client_details_form"
+        return "validate_details_form"
 
-    # @staticmethod
-    # def cuisine_db() -> List[Text]:
-    #     """Database of supported cuisines"""
-
-    #     return ["caribbean", "chinese", "french"]
-
-    def validate_client_name(
+    def validate_name(
         self,
-        value: Text,
+        slot_value: Any,
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: DomainDict,
-    ) -> Dict[Text, Any]:
-        """Validate client name value."""
+       ) -> Dict[Text, Any]:
+        """Validate name value."""
 
-        print(">>>>><<<<<<", tracker.get_slot('client_name'))
-        print("$$$$$$$$", value)
-
-        if len(value) == 0 or len(value) > 1:
+        slot_value = slot_value.strip()
+        print('>>>>>', slot_value.isdigit())
+        if len(slot_value) > 1 and not slot_value.isdigit():
             # validation succeeded, set the value of the "name" slot to value
-            return {"client_name": value}
+            return {"name": slot_value}
         else:
-            dispatcher.utter_message(response="utter_wrong_client_name")
+            dispatcher.utter_message(response="utter_wrong_name")
             # validation failed, set this slot to None so that the
             # user will be asked for the slot again
-            return {"client_name": None}
+            return {"name": None}
 
 
 # class ActionDefaultFallback(Action):
